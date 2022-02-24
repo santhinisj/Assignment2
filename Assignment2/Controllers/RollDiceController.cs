80e1da7d6f881feb72cf859d2318e505c5aac86d
Santhini Sasidharan <santhinisj@gmail.com>
2022-02-24 16:16:55 -0500

    J1 and j2 added

---
+++ Assignment2/Controllers/RollDiceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class RollDiceController : ApiController
+    {
+
+        // GET api/J2/DiceGame/6/8 returns "There are 5 ways to get the sum 10"
+
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        //method determines how many ways two dice can be rolled to get the value of 10
+        public string Get(int m, int n)
+        {
+            int noOfWaysToGet10 = 0;
+            for(int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <=n; j++)
+                {
+                    if(i+j == 10)
+                    {
+                        noOfWaysToGet10++;
+
+                    }
+                }
+            }
+            if(noOfWaysToGet10 == 1)
+            {
+                return "There is " + noOfWaysToGet10 + " ways to get the sum 10.";
+            }
+            else
+            {
+                return "There are " + noOfWaysToGet10 + " ways to get the sum 10.";
+            }
+           
+        }
+    }
+}
---
+++ Assignment2/Controllers/TotalCalorieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class TotalCalorieController : ApiController
+    {
+
+        // GET api/J1/Menu/4/4/4/4 --> returns 0
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        //This method accepts 4 index parameters and return the total calories
+        public string Get(int burger, int drink, int side, int dessert)
+        {
+            int[] burgerCalorieList = { 461, 431, 420, 0 };
+            int[] softDrinkCalorieList = { 130, 160, 118, 0 };
+            int[] sideCalorieList = { 100, 57, 70, 0 };
+            int[] dessertCalorieList = { 167, 266, 75, 0 };
+            int totalCalories = burgerCalorieList[burger-1]+ softDrinkCalorieList[drink-1] + sideCalorieList[side-1] + dessertCalorieList[dessert-1];
+            return "Your total calorie count is "+ totalCalories;
+        }
+
+    }
+}
